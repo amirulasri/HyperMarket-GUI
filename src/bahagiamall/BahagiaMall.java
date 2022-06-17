93dae587be80b6ffbf7d1c3b30b2f6af2fddaf56
amirulasri <amirulasrix@gmail.com>
2022-06-17 11:53:37 +0800

    Created Main UI
    Still not functioning, will be improved soon

--- src/bahagiamall/BahagiaMall.java
+++ src/bahagiamall/BahagiaMall.java
@@ -1,10 +1,70 @@
 package bahagiamall;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import gui.HomeUI;
+
 public class BahagiaMall {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new HomeUI().setVisible(true);
+    }
+    
+    public void outputtoexcel() throws FileNotFoundException, Exception{
+        // workbook object
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        // spreadsheet object
+        XSSFSheet spreadsheet = workbook.createSheet(" Student Data ");
+
+        // creating a row object
+        XSSFRow row;
+
+
+        Map<String, Object[]> studentData = new TreeMap<String, Object[]>();
+
+        studentData.put("1", new Object[]{"ID", "NAME"});
+        studentData.put("2", new Object[]{"1", "Ainul"});
+        studentData.put("3", new Object[]{"2", "Ain"});
+        studentData.put("4", new Object[]{"3", "Amy"});
+        studentData.put("5", new Object[]{"4", "Putra"});
+        Set<String> keyid = studentData.keySet();
+
+        int rowid = 0;
+
+        // writing the data into the sheets...
+        for (String key : keyid) {
+
+            System.out.println("Rows: "+rowid);
+            row = spreadsheet.createRow(rowid++);
+            Object[] objectArr = studentData.get(key);
+            int cellid = 0;
+
+            for (Object obj : objectArr) {
+                System.out.println("Cells: "+cellid);
+                Cell cell = row.createCell(cellid++);
+                cell.setCellValue((String) obj);
+            }
+        }
+
+        // .xlsx is the format for Excel Sheets...
+        // writing the workbook into the file...
+        FileOutputStream out = new FileOutputStream(
+                new File("C:\\Users\\amiru\\Downloads\\Test.xlsx"));
+
+        workbook.write(out);
+        out.close();
     }
 }
